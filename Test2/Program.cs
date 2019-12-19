c8ecf494613c3727e05c6b006813e5c7fc519943
峻廷 林 <sanyo7931@yahoo.com.tw>
2019-12-19 10:07:10 +0800

    測試

---
+++ Test2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sumloop(1000));
+        }
+        public static int sumloop(int x)
+        {
+            if (x==1)
+            {
+                return 1;
+
+            }
+            else if (x % 2 ==1)
+            {
+                return x + sumloop(x - 2);
+            }
+            else
+            {
+                return sumloop(x - 1);
+            }
+        }
+    }
+}
